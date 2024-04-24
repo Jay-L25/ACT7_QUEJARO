26a4b2f869b165e68947f09b01ef8420d8796bff
Jay-L25 <jaylquejaro@gmail.com>
2024-04-25 01:25:37 +0800

    Act7_Quejaro

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index (){
+        $products = "Product List Form in ProductController";
+        return view ('products.index', ['products'=>$products]);
+    }
+
+    public function create(){
+        return view ('products.create');
+    }
+}
